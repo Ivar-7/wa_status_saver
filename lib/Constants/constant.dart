1246a43ba67f5128e25eb7a19f7dbc43cac4ced3
Ivar-7 <leank6071@gmail.com>
2024-10-18 16:10:01 +0300

    constant

--- lib/Constants/constant.dart
+++ lib/Constants/constant.dart
@@ -1,4 +1,8 @@
 class AppConstants {
   static String whatsappPath =
-      "/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses";
+      "/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses";
+      // "/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses";
+      // "/storage/emulated/0/WhatsApp/Media/.Statuses";
+      // "/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses";
+      // "/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Video/.Statuses;
 }
